e98d5959ed0f12163fe2ffe4b6613e821d7cd425
DmitryyKaminskyy <drakon2000@ukr.net>
2017-12-06 21:46:02 +0200

    organization of development environment

--- app/js/vendor.js
+++ app/js/vendor.js
@@ -1,6 +1,5 @@
-//= vendor/modernizr.custom.js
-//= bower_components/bootstrap/dist/js/jquery.min.js
+//= vendor/jquery.min.js
+//= vendor/bootstrap.min.js
 //= vendor/jquery-migrate-3.0.0.min.js
-//= bower_components/bootstrap/dist/js/bootstrap.min.js
-//= bower_components/slick-carousel/slick/slick.js
-//= bower_components/wow/dist/wow.min.js
+//= vendor/slick.js
+//= vendor/wow.min.js
