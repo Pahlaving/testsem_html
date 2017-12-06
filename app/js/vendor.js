5bfb1547009a6deb65f09124c373bfc52f137098
DmitryyKaminskyy <drakon2000@ukr.net>
2017-12-06 20:49:09 +0200

    Add structure progect
    Add files and folders

---
+++ app/js/vendor.js
@@ -0,0 +1,5 @@
+//= bower_components/bootstrap/dist/js/jquery.min.js
+//= vendor/jquery-migrate-3.0.0.min.js
+//= bower_components/bootstrap/dist/js/bootstrap.min.js
+//= bower_components/slick-carousel/slick/slick.js
+//= bower_components/wow/dist/wow.min.js
