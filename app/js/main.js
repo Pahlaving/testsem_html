28720e354d93630fa19266eeb2d5197d12e608c9
DmitryyKaminskyy <drakon2000@ukr.net>
2017-12-07 19:03:56 +0200

    breadcrumbs adaptive

--- app/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$('.slider').click(function() {
-	  $(this).toggleClass('slider__toggle');
-	  $('.receive__guests__text').toggleClass('receive__guests__toggle');
-	  $('.receive__guests span').toggleClass('receive__guests__toggle');
-	});
-});
