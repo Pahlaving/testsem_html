0fac570d0da67558650f0c0d2ac68622391825ef
DmitryyKaminskyy <drakon2000@ukr.net>
2017-12-07 19:12:39 +0200

    js add

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$('.slider').click(function() {
+	  $(this).toggleClass('slider__toggle');
+	  $('.receive__guests__text').toggleClass('receive__guests__toggle');
+	  $('.receive__guests span').toggleClass('receive__guests__toggle');
+	});
+});
