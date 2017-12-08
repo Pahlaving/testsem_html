f71d8049e4cb9cf2d9f6a1bfae59adafaedb9de5
DmitryyKaminskyy <drakon2000@ukr.net>
2017-12-08 21:47:29 +0200

    popaps

--- app/js/main.js
+++ app/js/main.js
@@ -21,29 +21,9 @@ $(document).ready(function(){
 	$('input[type=file]').bootstrapFileInput();
   $('.file-inputs').bootstrapFileInput();
 
-  // $('.add-phone').on('click' ,function (e) {
-  //   e.preventDefault();
-  //   $('#add-phone-form').toggle();
-  // });
-  // $('.add-emergency-contact').on('click' ,function (e) {
-  //   e.preventDefault();
-  //   $('#emergency_contact_form').toggle();
-  // });
-
-  hiddenFormsToggl('.add-phone', '#add-phone-form');
-  hiddenFormsToggl('.add-emergency-contact', '#emergency_contact_form');
-
-  function hiddenFormsToggl(trigger, target) {
-    $(trigger).on('click' ,function (e) {
-      e.preventDefault();
-      $(target).toggle();
-    });
-  }
-
-
-  $('#login__popup #form .log .outline__chek').click(function (e) {
-    $("#login__popup #form .log input").attr('checked','checked');
-  });
-
+  $('.add-phone').on('click' ,function (e) {
+    e.preventDefault();
+    $('#add-phone-form').toggle();
+  })
 
 });
