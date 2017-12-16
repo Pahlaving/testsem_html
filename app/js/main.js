79f3073c5a617573f7b97b20d354055e404db4f2
DmitryyKaminskyy <drakon2000@ukr.net>
2017-12-16 12:44:13 +0200

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -60,7 +60,7 @@ $(document).ready(function(){
     items : 4, 
     autoplay: 3000,
     nav:false,
-    //pagination: true,
+    pagination: true,
     pagination: false,
     responsive:{
       0:{
